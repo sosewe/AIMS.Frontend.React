5a792f8d92e0581d290d6d74ae8b3a3b7a9065bf
felixkithinji <felixkithinji@outlook.com>
2023-01-22 14:31:28 +0300

    remove unused components

--- src/api/result-chain-indicator.js
+++ src/api/result-chain-indicator.js
@@ -4,3 +4,10 @@ import axios from "axios";
 export const saveResultChainIndicator = async (values) => {
   return await axios.post(`${apiRoutes.resultChainIndicator}`, values);
 };
+
+export const getResultChainIndicatorsByResultChainId = async ({ queryKey }) => {
+  const [, resultChainId] = queryKey;
+  return await axios.get(
+    `${apiRoutes.resultChainIndicator}/GetResultChainIndicatorByResultChainId/${resultChainId}`
+  );
+};
